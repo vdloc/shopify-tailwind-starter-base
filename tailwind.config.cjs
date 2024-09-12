f312e712bdc185311885c44c3be83abd159236db
vdloc <uvcudlco@gmail.com>
2024-09-12 23:21:43 +0700

    refactor(css): consolidate and simplify tailwind config, remove unnecessary CSS files
    Removed unnecessary CSS files (app-tailwind.css and app.css) and consolidated Tailwind configuration into a single file (tailwind.config.cjs). Updated various liquid templates to utilize the new CSS structure and ensure consistent styling across the application. This change simplifies the project's structure and improves maintainability.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   prefix: 'tw-',
-  content: [
-    './**/*.liquid',
-    './frontend/**/*.{js,ts,jsx,tsx}'
-  ],
+  content: ['./**/*.liquid', './frontend/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        worksans: '"Work Sans", sans-serif',
+        'league-spartan': '"League Spartan", sans-serif',
+      },
+      maxWidth: {
+        default: '1440px',
+      },
+      container: {
+        center: true,
+        padding: '2rem',
+        screens: {
+          sm: '100%',
+          md: '100%',
+          lg: '800px', // Custom width for large screens
+          xl: '1000px', // Custom width for extra-large screens
+          xlg: '1200px', // Custom width for 2xl screens
+          x2lg: '1440px', // Custom width for 2xl screens
+        },
+      },
+    },
   },
-  plugins: []
-}
+  screens: {
+    sm: '320px',
+    md: '750px',
+    lg: '990px',
+    xlg: '1440px',
+    x2lg: '1920px',
+    pageMaxWidth: '1440px',
+  },
+  plugins: [],
+};
