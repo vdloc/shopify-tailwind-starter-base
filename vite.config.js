d2a8a1873cbf48e11ef32b46bab5d1a7a94a883e
vdloc <uvcudlco@gmail.com>
2024-09-12 18:38:49 +0700

    feat(shopify-vite): integrate Vite with Shopify theme development
    - Added `vite.config.js` and `vite-plugin-shopify` to enable Vite in the project.
    - Updated `.gitignore` and `.shopifyignore` for new Vite-related files.
    - Introduced `tailwind.config.cjs` and `postcss.config.cjs` for Tailwind CSS configuration.
    - Modified various Liquid files to inject Vite tags for `main.css` and `main.js`.
    - Updated `package.json` scripts for development and deployment workflows using Vite.
    - Included a new logo image and main frontend entry points (`main.css` and `main.js`).
    - Adjusted template JSON files to include empty settings objects where necessary.

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+import shopify from 'vite-plugin-shopify';
+
+export default defineConfig({
+  plugins: [shopify()],
+  build: {
+    emptyOutDir: false,
+  },
+});
